7df0eb3ef8c0fdd80636e627d9aaa362d34389dd
Diwan03kawishka <96042733+Diwan03kawishka@users.noreply.github.com>
2021-12-16 11:14:52 +0530

    Update Mynumber.js

--- plugins/Mynumber.js
+++ plugins/Mynumber.js
@@ -4,14 +4,14 @@ const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Ktb.addCommand({ pattern: 'number ?(.*)', fromMe: false, desc: 'imash number' }, (async (message, match) => {
+Ktb.addCommand({ pattern: 'number ?(.*)', fromMe: false, desc: 'diwan number' }, (async (message, match) => {
 
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:IMASH NIDUSHA\n' // full name
-            + 'ORG:IMASH(Zed);\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=94771323434:+94 771323434\n' // WhatsApp ID + phone number
+            + 'FN:DIWAN KAWISHKA\n' // full name
+            + 'ORG:DIWAN(BHAGYA);\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=94786705379:+94 786705379\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "IMASH NIDUSHA", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "DIWAN KAWISHKA", vcard: vcard}, MessageType.contact)
 }))
